274f25e0d347034542c079b8e8bbdc75e276779b
Rituraj <Rituraj.Raman@evalueserve.com>
2024-05-26 03:01:09 +0530

    Commit

--- src/MyComponents/ComponentDidMount.jsx
+++ src/MyComponents/ComponentDidMount.jsx
@@ -1,7 +1,13 @@
-export default function ComponentDidMount() {
-    return (
-        <>
+import react from "react";
 
-        </>
-    )
+class ComponentDidMount extends react.Component {
+
+    render() {
+        return (
+            <>
+            </>
+        )
+    }
 }
+
+export default ComponentDidMount;
