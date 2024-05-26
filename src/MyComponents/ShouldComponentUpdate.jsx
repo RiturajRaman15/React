a5f80bd8c9b451c2749f52588a1427d50531a78f
Rituraj Raman <riturajraman12345@gmail.com>
2024-05-27 03:10:10 +0530

    Commit

--- src/MyComponents/ShouldComponentUpdate.jsx
+++ src/MyComponents/ShouldComponentUpdate.jsx
@@ -2,29 +2,28 @@ import react from "react";
 
 class ShouldComponentUpdate extends react.Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             count: 0
         }
         console.log("Constructor");
     }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log("ComponentDidUpdate", prevState);
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        console.log("Should Component Update");
+        return this.state.count < 3;
     }
 
     render() {
         console.log("Render");
-        return (
-            <>
-                <h1>Should Component Update</h1>
-                <h6>Check Console</h6>
-                <h3>Hello {this.state.count}</h3>
-                <button onClick={() => this.setState({count: this.state.count + 1})}>Change</button>
-                <h4>Should Component update...</h4>
-            </>
-        )
+        return (<>
+            <h1>Should Component Update</h1>
+            <h6>Check Console</h6>
+            <h3>Hello {this.state.count}</h3>
+            <button onClick={() => this.setState({count: this.state.count + 1})}>Change</button>
+            <h4>Conditional rendering of component based on state</h4>
+        </>)
     }
 }
 
